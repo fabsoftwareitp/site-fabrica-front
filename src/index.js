4be519acfc0dc5cc2855ca6d009892bcf78e8bd3
danilocbueno <danilocbueno@gmail.com>
2020-09-16 20:29:05 -0300

    transformar em pwa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as serviceWorker from './serviceWorker'
+
 import App from './App';
 
 
@@ -11,3 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorker.registerServiceWorker();
