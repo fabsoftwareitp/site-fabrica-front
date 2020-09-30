18cb1c2b6ef0c6466006a17313b5dbe143969275
danilocbueno <danilocbueno@gmail.com>
2020-09-30 16:30:33 -0300

    Updates

---
+++ precache-manifest.3b30f853fc142eaced6428a9e2021ff7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55de11f770af413c410f6c2c1517fb2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b6112ee384d420dc440",
+    "url": "./static/js/2.7ef44c77.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.7ef44c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d1ae9fbfe891495404",
+    "url": "./static/js/main.54563f01.chunk.js"
+  },
+  {
+    "revision": "788bfe92f3ed1660f148",
+    "url": "./static/js/runtime-main.fe9c274e.js"
+  },
+  {
+    "revision": "7e48df29ead2e07b5b846ced885700f7",
+    "url": "./static/media/Logo.7e48df29.png"
+  },
+  {
+    "revision": "9f0e03898ef5f3339950e3475729afff",
+    "url": "./static/media/Time.9f0e0389.jpeg"
+  },
+  {
+    "revision": "e44d4dd6594e6ae6718a89059f29cdc7",
+    "url": "./static/media/facebook.e44d4dd6.svg"
+  },
+  {
+    "revision": "3f9623e5735b190a7960308ca5157cc8",
+    "url": "./static/media/github.3f9623e5.svg"
+  },
+  {
+    "revision": "dda4ed64a67ac5d80e8eac27dfbe677e",
+    "url": "./static/media/github.dda4ed64.png"
+  }
+]);
