eb4b8fed9575e6f3fa481de4123f9cfde331219d
joaomorais1f <ifsp.joaov@gmail.com>
2020-11-04 15:14:58 -0300

    estilos para a Home

---
+++ src/screens/Home/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    max-width: 1920px;
+    margin: 0 auto;
+    overflow-x: hidden;
+`;
+
+export const NewsButton = styled.button`
+    display: block;
+    max-width: 100%;
+    width: 250px;
+    height: 50px !important;
+    background-color: var(--circle-color);
+    padding: 10px 8px;
+    border-radius: 10px;
+    margin: 0 auto 10px auto;
+    cursor: pointer;
+    &:hover {
+        background-color: #496afc;
+    }
+    transition: .5s;
+    color: rgba(255,255,255,1);
+        /* font-weight: bold; */
+
+`;
